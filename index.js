549041c2eeef232fce459f6815bf5217257120bd
Dio Audia <76801729+dioaudr@users.noreply.github.com>
2021-02-09 10:07:06 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let kata = 'Stefani';
+console.log(`Username ${kata}`)
+
+let kata1 = 25;
+console.log(`age ${kata1}`)
+
+let kata2 = 0
+if(!kata2){
+console.log('Invalid data')
+} else {
+    console.log(kata2)
+}
+
+let kata3 = '0';
+console.log(`Username ${kata3}`)
+
+let kata4 = false
+if(kata4 !== true){
+    console.log('cannot proceed without agreement')
+} else {
+    console.log('thank you for agreeing')
+}
